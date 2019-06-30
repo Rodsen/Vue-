2bf39361d3920e7de68e8bc67518eaa7caa6c06e
Rodsen <rodsen1995@gmail.con>
2019-07-01 07:04:41 +0800

    修改报错

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 Vue.use (VueRouter)
 import router from "./router"
-//----------------------------样式区
-//  引入全局样式
-import "./css/base.css"
 //------------------------------底部区
 //  引入首页渲染组件
 import App from "./App.vue"
